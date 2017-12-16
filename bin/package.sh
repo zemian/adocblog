94dc4bb0e7bda3150b561b7352cf215c9a246e76
Zemian Deng <zemiandeng@gmail.com>
2017-12-16 10:42:29 -0500

    Improve release package

--- bin/package.sh
+++ bin/package.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #
-# Zip up package for remote deployment.
+# Zip up a package for remote deployment. This script will perform the following main tasks:
+#  - Generate a release.properties as part of the packaged application jar
+#  - Generate a skinny war
+#  - Generate all dependency jars in lib directory
+#  - Move all app*.properties files into config directory
 #
 # Usage: package.sh [GIT_TAG_NAME]
 #   The optional argument GIT_TAG_NAME is default to 'HEAD'.
@@ -34,7 +38,7 @@ echo "Release file created: $RELEASE_FILE"
 cat $RELEASE_FILE
 
 # Maven artifact package
-mvn clean package -DskipTests -Ppackage-no-app-props || exit 1
+mvn clean package -DskipTests -Ppackage-zip || exit 1
 
 # Clean up
 rm -vf $RELEASE_FILE
@@ -58,6 +62,11 @@ cp -rvf $PROJ_HOME/src/main/resources/adocblog/app*.properties $PACKAGE_DIR/conf
 # Copy war
 FINAL_PKG_FILE=$PACKAGE_DIR/adocblog-$VERSION.war
 cp -vf $PROJ_HOME/target/adocblog.war $FINAL_PKG_FILE
+
+# Copy dependency
+mkdir -p $PACKAGE_DIR/lib
+cp -vf $PROJ_HOME/target/dependency/* $PACKAGE_DIR/lib
+cp -vf $PROJ_HOME/target/adocblog/WEB-INF/lib/* $PACKAGE_DIR/lib
 
 # Zip it up
 zip -r $PACKAGE_DIR.zip $PACKAGE_DIR
