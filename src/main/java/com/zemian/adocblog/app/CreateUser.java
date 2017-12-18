d2c61bfdba6f7b73bd33b1d2abe97a32a214a229
Zemian Deng <zemiandeng@gmail.com>
2017-12-18 08:13:08 -0500

    Add encryptPassword option usage

--- src/main/java/com/zemian/adocblog/app/CreateUser.java
+++ src/main/java/com/zemian/adocblog/app/CreateUser.java
@@ -3,6 +3,7 @@ package com.zemian.adocblog.app;
 import com.zemian.adocblog.AppException;
 import com.zemian.adocblog.CommonConfig;
 import com.zemian.adocblog.app.support.CmdOpts;
+import com.zemian.adocblog.cipher.Crypto;
 import com.zemian.adocblog.data.domain.User;
 import com.zemian.adocblog.service.ServiceConfig;
 import com.zemian.adocblog.service.UserService;
@@ -42,42 +43,66 @@ public class CreateUser {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private Crypto crypto;
+
+    private void printHelp() {
+        System.out.println("User management tool.\n" +
+                "\n" +
+                "Usage: (create new user)\n" +
+                "  java [--adminUser=true] [--fullName=NAME] <username> <password>\n" +
+                "\n" +
+                "Usage: (update new user)\n" +
+                "  java --update [--adminUser=true] [--fullName=NAME] <username> <password>\n" +
+                "\n" +
+                "Usage: (print encrypted password only)\n" +
+                        "  java --encryptPassword=PASSWORD\n"
+        );
+    }
+
     public void run(String[] args) {
         CmdOpts opts = new CmdOpts(args);
 
-        if (opts.getArgsSize() < 2) {
-            throw new AppException("Wrong args:" +
-                    " java [--adminUser=true] [--fullName=NAME] <username> <password>");
-        }
-
-        String username = opts.getArg(0);
-        String password = opts.getArg(1);
-
-        boolean isAdmin = opts.getBooleanOpt("adminUser", false);
-        String fullName = opts.getOpt("fullName", "User " + username);
-
-        if(opts.hasOpt("update")) {
-            LOG.debug("Updating existing user: {}", username);
-
-            User user = userService.get(username);
-            user.setPassword(password);
-            user.setAdmin(isAdmin);
-            user.setFullName(fullName);
-
-            userService.update(user);
-
-            LOG.info("{} (fullName={}) has been updated successfully.", user, fullName);
+        if(opts.hasOpt("encryptPassword")) {
+            String password = opts.getOpt("encryptPassword");
+            String encryptedPassword = crypto.encrypt(password);
+            System.out.println("Password: " + encryptedPassword);
+            System.exit(0);
         } else {
-            LOG.debug("Creating new user: {}", username);
+            if (opts.getArgsSize() < 2) {
+                throw new AppException("Wrong args:" +
+                        " ");
+            }
 
-            User user = new User();
-            user.setUsername(username);
-            user.setPassword(password);
-            user.setAdmin(isAdmin);
-            user.setFullName(fullName);
-            userService.create(user);
+            String username = opts.getArg(0);
+            String password = opts.getArg(1);
 
-            LOG.info("{} (fullName={}) has been created successfully.", user, fullName);
+            boolean isAdmin = opts.getBooleanOpt("adminUser", false);
+            String fullName = opts.getOpt("fullName", "User " + username);
+
+            if (opts.hasOpt("update")) {
+                LOG.debug("Updating existing user: {}", username);
+
+                User user = userService.get(username);
+                user.setPassword(password);
+                user.setAdmin(isAdmin);
+                user.setFullName(fullName);
+
+                userService.update(user);
+
+                LOG.info("{} (fullName={}) has been updated successfully.", user, fullName);
+            } else {
+                LOG.debug("Creating new user: {}", username);
+
+                User user = new User();
+                user.setUsername(username);
+                user.setPassword(password);
+                user.setAdmin(isAdmin);
+                user.setFullName(fullName);
+                userService.create(user);
+
+                LOG.info("{} (fullName={}) has been created successfully.", user, fullName);
+            }
         }
     }
 }
