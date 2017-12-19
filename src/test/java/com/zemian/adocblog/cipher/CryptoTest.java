d0a4d562734ae07698d562ff3f6c54e4b7e1dce3
Zemian Deng <zemiandeng@gmail.com>
2017-12-18 21:09:15 -0500

    Improve crypto test

--- src/test/java/com/zemian/adocblog/cipher/CryptoTest.java
+++ src/test/java/com/zemian/adocblog/cipher/CryptoTest.java
@@ -1,6 +1,7 @@
 package com.zemian.adocblog.cipher;
 
 import com.zemian.adocblog.SpringTestBase;
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,14 +16,17 @@ public class CryptoTest extends SpringTestBase {
 
     @Test
     public void generateIDs() {
-        assertThat(Crypto.randString(16).length(), is(16));
-        assertThat(Crypto.randString(16).length(), is(16));
-        assertThat(Crypto.randString(16).length(), is(16));
+        MatcherAssert.assertThat(Crypto.randString(16).length(), is(16));
+        MatcherAssert.assertThat(Crypto.randString(32).length(), is(32));
+        MatcherAssert.assertThat(Crypto.randString(64).length(), is(64));
+        MatcherAssert.assertThat(Crypto.randString(128).length(), is(128));
+        MatcherAssert.assertThat(Crypto.randString(256).length(), is(256));
     }
 
     @Test
     public void encrypt() {
         String secret = crypto.encrypt("test");
-        assertThat(crypto.decrypt(secret), is("test"));
+        //System.out.println(secret);
+        MatcherAssert.assertThat(crypto.decrypt(secret), is("test"));
     }
 }
