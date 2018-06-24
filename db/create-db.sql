735055564b93730801ff94772cb17c97ee2ea5f5
Zemian Deng <zemiandeng>
2018-06-24 13:38:26 -0400

    Simply init db instructions

--- db/create-db.sql
+++
@@ -1,4 +0,0 @@
--- We should execute this script with 'superuser'
--- This script prepare a DB user 'adocblog' and a empty database for app use.
-CREATE USER adocblog;
-CREATE DATABASE adocblog_dev WITH OWNER = adocblog;
