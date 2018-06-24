27a1cf022ec231f53dddcb7c1cad09ec7ce1b983
Zemian Deng <zemiandeng>
2018-06-24 09:58:42 -0400

    Fix deault db name

--- bin/init-db.sh
+++ bin/init-db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Setup and initialize a new PostgreSQL DB named `adocblog` and a DB userid=`adocblog`.
+# Setup and initialize a new PostgreSQL DB named `adocblog_dev` and a DB userid=`adocblog`.
 #
 # Usage:
 #  init-db.sh
@@ -14,4 +14,4 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 psql -U postgres -f $SCRIPT_DIR/../db/create-db.sql
 
 # Create schema
-psql -U adocblog -f $SCRIPT_DIR/../db/create-schema.sql
+psql -U adocblog -f $SCRIPT_DIR/../db/create-schema.sql adocblog_dev
